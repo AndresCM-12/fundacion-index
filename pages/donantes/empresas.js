6841ac3b9fde8db0866872b365eba478932817ec
Andres <andres@deadline.com.mx>
2023-01-03 10:54:47 -0700

    Fix: donantes page and added loading response in each page

--- pages/donantes/empresas.js
+++ pages/donantes/empresas.js
@@ -4,6 +4,7 @@ import MainLayout from "@/components/layouts/MainLayout";
 import ImageSection from "@/components/sections/ImageSection";
 import Image from "next/image";
 import DonantesBanner from "@/components/banners/DonantesBanner";
+import { getCategories, getPostsFromCategories } from "@/components/utils/Queries";
 
 const data = {
   partone: [
@@ -368,7 +369,7 @@ const data = {
   ],
 };
 
-const EmpresasPage = () => {
+const EmpresasPage = ({ mainLayoutNavData }) => {
   const CompanyItem = ({ item, key }) => {
     return (
       <div
@@ -404,7 +405,7 @@ const EmpresasPage = () => {
   };
 
   return (
-    <MainLayout title="Empresas recicladoras">
+    <MainLayout navData={mainLayoutNavData} title="Empresas recicladoras">
       <DonantesBanner className="bg-index-aqua" />
       <div className="sectioncontainer w-full flex justify-center bg-index-gray pb-16">
         <div className="wrapper max-w-7xl flex flex-col lg:flex-row justify-center w-full mt-20">
@@ -439,6 +440,114 @@ const EmpresasPage = () => {
       <ImageSection image="/images/friendspic.jpg" type="programas" />
     </MainLayout>
   );
+};
+
+export async function getServerSideProps({ params }) {
+  const mainLayoutNavData = await getSections();
+  if (!data) {
+    return {
+      props: {
+        mainLayoutNavData: [
+          {
+            text: "Server error",
+            link: "/",
+          },
+        ],
+        postIndex: 0,
+      },
+    };
+  }
+
+  return {
+    props: {
+      mainLayoutNavData: mainLayoutNavData,
+    },
+  };
+}
+
+async function getSections() {
+  const response = await getCategories();
+  if (!response) {
+    return {
+      nosotrosSection: { dropdown: [] },
+      programasSection: { dropdown: [] },
+      donativosSection: { dropdown: [] },
+      recicladoresSection: { dropdown: [] },
+    };
+  }
+  let navData = [];
+  let nosotrosSection;
+  let programasSection;
+  let donativosSection;
+  let recicladoresSection;
+  let boletinesSection;
+  let props;
+
+  await Promise.all(
+    response.data.categories.edges.map(async (category) => {
+      let nav = await fetchTitles(category.node.name);
+      navData.push({ title: category.node.name, dropdown: nav });
+
+      nosotrosSection = navData.find((item) => {
+        return item.title === "nosotros";
+      });
+      programasSection = navData.find((item) => {
+        return item.title === "programas";
+      });
+      donativosSection = navData.find((item) => {
+        return item.title === "donativos";
+      });
+      recicladoresSection = navData.find((item) => {
+        return item.title === "recicladores";
+      });
+      boletinesSection = navData.find((item) => {
+        return item.title === "boletines";
+      });
+      return await {
+        nosotrosSection,
+        programasSection,
+        donativosSection,
+        recicladoresSection,
+        boletinesSection,
+      };
+    })
+  );
+
+  props = {
+    nosotrosSection,
+    programasSection,
+    donativosSection,
+    recicladoresSection,
+    boletinesSection,
+  };
+  return await props;
+}
+
+const fetchTitles = async (category) => {
+  const data = await getPostsFromCategories(category);
+  if (!data) return [{ text: "Error, recarga la página", link: "/" }];
+  const posts = data.data.categories.nodes[0].posts.edges;
+  const categoryTitle = data.data.categories.edges[0].node.name;
+
+  const navData = posts.map((post) => {
+    if (post.node.title === "Requisitos" && categoryTitle === "recicladores") {
+      return {
+        text: post.node.title,
+        link: `/${categoryTitle}/Empresas%20recicladoras#requisitos`,
+      };
+    }
+    if (post.node.title === "F-30" && categoryTitle === "recicladores") {
+      return {
+        text: post.node.title,
+        link: `/${categoryTitle}/Documentación#f30`,
+      };
+    }
+    return {
+      text: post.node.title,
+      link: `/${categoryTitle}/${post.node.title}`,
+    };
+  });
+  return navData;
 };
 
 export default EmpresasPage;
